e1731cbfc78f22a028fe643b39afe0fa328177d8
longvan21411 <longvan21411@gmail.com>
2026-04-27 10:32:57 +0700

    get latest commit message

--- GithubConfiguration/obj/Debug/net8.0/GithubConfiguration.AssemblyInfo.cs
+++ GithubConfiguration/obj/Debug/net8.0/GithubConfiguration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GithubConfiguration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba2722e94d12efc4308a211f0d9643596d96ba48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e020dd6d7813ede30d3b376bbe5463de7d93b67")]
 [assembly: System.Reflection.AssemblyProductAttribute("GithubConfiguration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GithubConfiguration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
