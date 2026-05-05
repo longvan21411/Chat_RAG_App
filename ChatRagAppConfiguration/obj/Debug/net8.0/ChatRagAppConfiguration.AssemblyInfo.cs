0cad87879a8718a8944979f69874948f7789272b
Van Ta Van <tvv2hc@bosch.com>
2026-05-05 16:05:46 +0700

    refactor the Chat UI

--- ChatRagAppConfiguration/obj/Debug/net8.0/ChatRagAppConfiguration.AssemblyInfo.cs
+++ ChatRagAppConfiguration/obj/Debug/net8.0/ChatRagAppConfiguration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatRagAppConfiguration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f062549442dc67bde12d0b2ef2e15dc7c0fee62b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa74f763209d1934017df90cbc8d700a48d4ad51")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatRagAppConfiguration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatRagAppConfiguration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
