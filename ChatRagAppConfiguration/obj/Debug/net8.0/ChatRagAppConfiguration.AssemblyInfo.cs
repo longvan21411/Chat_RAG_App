7ef1a97123830667d1a14973740ad4c3fbe2be01
longvan21411 <longvan21411@gmail.com>
2026-05-06 22:48:53 +0700

    added image embedding related components and updated configuration

--- ChatRagAppConfiguration/obj/Debug/net8.0/ChatRagAppConfiguration.AssemblyInfo.cs
+++ ChatRagAppConfiguration/obj/Debug/net8.0/ChatRagAppConfiguration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatRagAppConfiguration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cad87879a8718a8944979f69874948f7789272b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4cf48d93e516b4f3ad39e9947e730ab85815826")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatRagAppConfiguration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatRagAppConfiguration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
